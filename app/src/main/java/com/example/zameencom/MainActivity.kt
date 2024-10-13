27544f1475cfbed6f7f6ea9edec4352f0a56a786
Ayman Ashraff <aymanashrafbskk@gmail.com>
2024-10-13 20:42:32 +0500

    new

---
+++ app/src/main/java/com/example/zameencom/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.zameencom
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Enables edge-to-edge layout
+        enableEdgeToEdge()
+
+        // Set the layout to your login activity
+        setContentView(R.layout.activity_login)
+
+        // Adjust padding to avoid content being hidden by system bars
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
